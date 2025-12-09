9bf2c22c2a735b58cda7a4effc98488a0987d9e4
ninjastorm24 <siamabdullah1100@gmail.com>
2025-12-09 16:45:51 +0600

    excel export

--- app/(marketing)/b2c-leads/page.tsx
+++ app/(marketing)/b2c-leads/page.tsx
@@ -1,8 +1,9 @@
-import { Button } from '@/components/ui/button';
-import { CirclePlus, Import } from 'lucide-react';
-import Link from 'next/link';
-import B2CLeadsTable from './_components/b2c-leads-table';
-import { Suspense } from 'react';
+import { Button } from "@/components/ui/button";
+import { CirclePlus, Import, FolderUp } from "lucide-react";
+import Link from "next/link";
+import B2CLeadsTable from "./_components/b2c-leads-table";
+import { Suspense } from "react";
+import ExcelExport from "./_components/excel-export";
 
 const page = () => {
   return (
@@ -10,14 +11,15 @@ const page = () => {
       <div className="flex items-center justify-between">
         <h2 className="text-2xl font-semibold tracking-tight">B2C Leads</h2>
         <div className="space-x-2">
-          <Button asChild variant={'outline'}>
-            <Link href={'/b2c-leads/bulk-import'}>
+          <ExcelExport />
+          <Button asChild variant={"outline"}>
+            <Link href={"/b2c-leads/bulk-import"}>
               <Import />
               Bulk Import
             </Link>
           </Button>
           <Button asChild>
-            <Link href={'/b2c-leads/create'}>
+            <Link href={"/b2c-leads/create"}>
               <CirclePlus />
               Create
             </Link>
